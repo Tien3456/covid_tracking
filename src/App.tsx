653f8bfafa3865ac18c96b1121cb7e2e92a34273
Tiến Trần <iamtien3456@gmail.com>
2022-05-11 17:48:35 +0700

    test api, create page, component files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import covidTrackingRepository from './repositories/CovidTrackingRepository'
+import { IDiseaseStatusOfCountry } from './interfaces/diseaseStatus'
 
 function App() {
 
+  const [countries, setCountries] = useState<IDiseaseStatusOfCountry[]>([])
+
+  const fetchGetCountries = useRef<null | ReturnType<typeof setInterval>>(null)
+
   useEffect(() => {
+    const MILLIS_UPDATING = 3 * 60 * 1000
+    fetchGetCountries.current = setInterval(() => {
+      covidTrackingRepository
+        .getAll()
+        .then(res => setCountries(res.data))
+        .catch(err => {})
+    }, MILLIS_UPDATING)
+    
+    return () => {
+      if(fetchGetCountries.current) {
+        clearInterval(fetchGetCountries.current)
+      }
+    }
   }, [])
 
   return (
