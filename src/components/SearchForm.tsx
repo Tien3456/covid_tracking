6811c7323cb9d0bf6687f6a0d80036d86012e64c
Tiến Trần <iamtien3456@gmail.com>
2022-05-12 09:21:46 +0700

    handle search input change

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,6 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const SearchForm = () => {
+
+    const navigate = useNavigate()
+
+    const [country, setCountry] = useState<string>('')
+
+    useEffect(() => {
+        country
+            ? navigate({
+                pathname: '/search',
+                search: `?country=${country}`
+            })
+            : navigate('/')
+    }, [country])
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => setCountry(e.currentTarget.value)
 
     return (
         <div className="search-form container">
@@ -9,6 +25,8 @@ const SearchForm = () => {
                     <input 
                         type="text" name="country" placeholder="Search 667 regions..." 
                         className="d-inline-block pl-48 py-16 pr-16 txt-grey-dark-color font-size-18"
+                        onChange={ onChange }
+                        value={ country }
                     />
                     <div className="icon-wrapper">
                         <i className="fa-solid fa-magnifying-glass txt-grey-dark-color font-size-14"></i>
